b393f25d4010d6fc4a7cc2d1bdc973c1e5457277
Emil George James <emilgeorgejames@gmail.com>
2016-12-07 12:38:40 +0530

    Added the Debug Toolbar’s URLs in urls.py

--- src/project_name/urls.py
+++ src/project_name/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 import profiles.urls
 import accounts.urls
 from . import views
+
 
 urlpatterns = [
     url(r'^$', views.HomePage.as_view(), name='home'),
@@ -13,6 +16,11 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(accounts.urls, namespace='accounts')),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
 
 # User-uploaded files like profile pics need to be served in development
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
